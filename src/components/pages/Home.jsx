150913e44666efe3c6d5a87ec619239537f17b46
Jessany Kaline <jessany42@gmail.com>
2022-11-10 22:09:19 -0300

    FEAT home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import logo from "../../images/logo.svg"
+import React from "react";
+import logo from "../../images/logo.svg";
 
 export default function Home() {
+  const [users, setUsers] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState("");
+
+  const handleUserChange = () => {
+    console.log("handleUserChange");
+  };
+
+  const handleConfirmClick = () => {
+    console.log("handleConfirmCLick");
+  };
+
   return (
     <div className="home center">
-    <div className="home__logo">
-      <img src={logo} className="responsive" alt="" />
+      <div className="home__logo">
+        <img src={logo} className="responsive" alt="" />
+      </div>
+      <select onChange={handleUserChange} className="home__select-users">
+        <option>User 1</option>
+        <option>User 2</option>
+        <option>User 3</option>
+      </select>
+      <button onClick={handleConfirmClick} className="button-primary">
+        Entrar
+      </button>
     </div>
-    <select className="home__select-users">
-      <option>User 1</option>
-      <option>User 2</option>
-      <option>User 3</option>
-    </select>
-    <button className="button-primary">Entrar</button>
-  </div>
   );
 }
